6de08e05f281d2d1d85f377b8f10c00d0b407fce
Heydon <heydon@heydonworks.com>
2015-04-01 12:19:18 +0100

    gitignore rules + nested package management

--- ESA/Gruntfile.js
+++ ESA/Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt){
 
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+    require("matchdep").filterAll("grunt-*").forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
