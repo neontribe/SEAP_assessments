9dea039ab036c301482812f196af7c6755fbf0d8
katjam <katja@neontribe.co.uk>
2015-05-26 12:15:49 +0100

    init test localhost

--- ESA/src/tests/init-test.js
+++ ESA/src/tests/init-test.js
@@ -1,19 +1,10 @@
-// googletesting.js
-casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
-    casper.start("http://www.google.fr/", function() {
-        test.assertTitle("Google", "google homepage title is the one expected");
-        test.assertExists('form[action="/search"]', "main form is found");
-        this.fill('form[action="/search"]', {
-            q: "casperjs"
-        }, true);
-    });
-
-    casper.then(function() {
-        test.assertTitle("casperjs - Recherche Google", "google title is ok");
-        test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
-        test.assertEval(function() {
-            return __utils__.findAll("h3.r").length >= 10;
-        }, "google search for \"casperjs\" retrieves 10 or more results");
+casper.test.begin('Title page', 1, function suite(test) {
+    casper.start("http://seap-app", function() {
+        test.assertTitle("My ESA Assessment", "ESA page title is the one expected");
+        //test.assertExists('form[action="/search"]', "main form is found");
+        //this.fill('form[action="/search"]', {
+            //q: "casperjs"
+        //}, true);
     });
 
     casper.run(function() {
